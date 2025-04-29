77adfca1ef34a101b10e455a5c1a3735139b77d6
francisco-q <quevedof98@gmail.com>
2025-04-29 00:29:13 -0400

    inicialización de flask

---
+++ Backend/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+@app.route('/')
+def index():
+    return 'API para Tablero LED'
+
+if __name__ == '__main__':
+    app.run(debug=True)
